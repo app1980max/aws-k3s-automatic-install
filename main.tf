dc8a64b007f5856bacf9df9b9a1f7eb8bd10f140
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-16 18:19:10 +0300

    update

--- main.tf
+++ main.tf
@@ -1,35 +1,38 @@
+
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
-    bucket               = "backups-cluster-prod"
-    key                  = "states/terraform.tfstate"
-    region               = "eu-central-1"
-    dynamodb_table       = ""
-    #workspace_key_prefix = "env"
+    bucket         = "backups-cluster-prod"
+    key            = "states/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-locks"
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  version = "~> 5.0"
-  #version = "~> 4.65.0 "
-  region  = var.region
+  region = var.region
 }
 
 module "aws_infrastructure" {
   source = "./aws"
 
-  db_password = var.db_password
-  db_username = var.db_username
-  public_ssh_key = var.public_ssh_key
+  db_password        = var.db_password
+  db_username        = var.db_username
+  public_ssh_key     = var.public_ssh_key
   key_s3_bucket_name = var.key_s3_bucket_name
 
   k3s_server_count = var.k3s_server_count
-  k3s_agent_count = var.k3s_agent_count
+  k3s_agent_count  = var.k3s_agent_count
 
   k3s_server_size = var.k3s_server_size
-  k3s_agent_size = var.k3s_agent_size
-
-  configure_aws_provider = var.configure_aws_provider
+  k3s_agent_size  = var.k3s_agent_size
 
   cluster_name = var.cluster_name
 }
+
