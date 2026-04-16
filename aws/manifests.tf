8a8bd327f766e61ce4400b548254fd19ff05af1f
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-16 18:35:29 +0300

    update

--- aws/manifests.tf
+++ aws/manifests.tf
@@ -1,12 +1,15 @@
+
 variable "upload_directory" {
   default = "charts/"
 }
 
-resource "aws_s3_bucket_object" "website_files" {
-  for_each      = fileset(var.upload_directory, "**/*.*")
-  bucket        = var.key_s3_bucket_name
-  key           = replace(each.value, var.upload_directory, "apps/")
-  source        = "${var.upload_directory}${each.value}"
-  acl           = "private"
-  etag          = filemd5("${var.upload_directory}${each.value}")
+resource "aws_s3_object" "website_files" {
+  for_each = fileset(var.upload_directory, "**/*.*")
+
+  bucket = var.key_s3_bucket_name
+
+  key    = "apps/${each.value}"
+  source = "${var.upload_directory}${each.value}"
+
+  etag = filemd5("${var.upload_directory}${each.value}")
 }
